96e07158b059645a38f0923f79db47ae2d7c2bf1
adisak <adisak@gmail.com>
2023-03-15 13:53:40 -0500

    Bug Fixes
    Bug fixes: Change working directory properly and more correct behavior on when to open in a new window

--- scripts/CH_Config.bat
+++ scripts/CH_Config.bat
@@ -5,17 +5,33 @@ rem		for the CMD_Here.bat
 REM	Copyright (c) 2023 Adisak Pochanayon (adisak@gmail.com)
 REM -------------------------------------------------------------------
 
+REM call :SetToFullyExpandedPath SCRIPT_PATH "%~dp0"
+
+REM -------------------------------------------------------------------
+
+GOTO :SKIP_CONFIGURATION_MESSAGE
+if NOT "%CH_CONFIG_HAS_RUN%"=="1" (
+	echo Setting up CMD_Here configuration
+) else (
+	echo CMD_Here has already been configured
+)
+:SKIP_CONFIGURATION_MESSAGE
+
+REM -------------------------------------------------------------------
+
+REM Optional line below to prevent re-running configuration
+if "%CH_CONFIG_HAS_RUN%"=="1" GOTO :EXIT_BAT
+
 REM This section of the BATCH File can be modified by user for
 REM however they'd like to configure their working environement
-
-REM call :SetToFullyExpandedPath SCRIPT_PATH "%~dp0"
 
 REM Set the Text Color to Green on a Black Background
 COLOR 0A
 
 REM -------------------------------------------------------------------
 
-:EXITBAT
+:EXIT_BAT
+SET CH_CONFIG_HAS_RUN=1
 GOTO:EOF
 
 REM ---------------------------------------------
@@ -29,7 +45,7 @@ goto:EOF
 REM ---------------
 REM Clean up CH_* variables from CMD_Here
 :ClearCHVars
-set CH_WINDOW=
+set CH_OPEN_NEW_WINDOW=
 set CH_TITLE=
 set CH_CONFIG=
 set CH_WINDOW_DIR=
--- scripts/CMD_Here.bat
+++ scripts/CMD_Here.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-SETLOCAL
+SETLOCAL ENABLEEXTENSIONS ENABLEDELAYEDEXPANSION
 REM -------------------------------------------------------------------
 REM Helper Batch File to start a CMD Window with current directory set
 REM		to same directory as this Batch File
@@ -8,25 +8,31 @@ REM -------------------------------------------------------------------
 
 call :SetToFullyExpandedPath SCRIPT_PATH "%~dp0"
 
-SET CH_WINDOW=/B
+REM Use 'SET CH_OPEN_NEW_WINDOW=1' if you always want to open a new window
+REM Use 'SET CH_OPEN_NEW_WINDOW=0' if you want to reuse CMD windows when you call
+REM		CMD_Here.bat for the first time (subsequent calls will open new windows)
+SET CH_OPEN_NEW_WINDOW=0
 if "%IS_CH_WINDOW%"=="1" (
-	SET CH_WINDOW=
+	REM If we're already in a CMD_Here window, then open a new one
+	SET CH_OPEN_NEW_WINDOW=1
 )
 SET IS_CH_WINDOW=1
 
 REM -------------------------------------------------------------------
 
+REM Optional: Override Command Line Working Window Title Here
 REM set CH_TITLE=Preset Window Name
 set CH_TITLE=
-if NOT "%CH_TITLE%"=="" GOTO :DONE_WITH_TITLE
 
-REM Set window title to last path of current directory
+REM Set the CH Directory variable (CH_WINDOW_DIR)
+REM If Window Title isn't specified, set it to last path of current directory or script path as fallback
 set CH_WINDOW_DIR=%SCRIPT_PATH%
 if "%CH_WINDOW_DIR:~-1%" == "\" set CH_WINDOW_DIR=%CH_WINDOW_DIR:~0,-1%
 if "%CH_WINDOW_DIR:~-1%" == ":" (
 	set CH_WINDOW_DIR=%SCRIPT_PATH%
-	GOTO :DEFAULT_TITLE
+	if "%CH_TITLE%"=="" GOTO :DEFAULT_TITLE
 )
+if NOT "%CH_TITLE%"=="" GOTO :DONE_WITH_TITLE
 for %%I in ("%CH_WINDOW_DIR%") do set CH_TITLE=%%~nxI
 if NOT "%CH_TITLE%"=="" GOTO :DONE_WITH_TITLE
 
@@ -41,15 +47,29 @@ REM -------------------------------------------------------------------
 
 set CH_CONFIG=%SCRIPT_PATH%\CH_Config.bat
 
+if "%CH_OPEN_NEW_WINDOW%"=="1" GOTO :OPEN_NEW_WINDOW
+
+:USE_EXISTING_WINDOW
 if EXIST "%CH_CONFIG%" (
-	call :ClearCHVars & start %CH_WINDOW% "%CH_TITLE%" CMD /K TITLE %CH_TITLE% ^&^& "%CH_CONFIG%"
+	call :ClearCHVars & CMD /K TITLE %CH_TITLE% ^& cd /D "%CH_WINDOW_DIR%" ^& "%CH_CONFIG%"
 ) else (
-	call :ClearCHVars & start %CH_WINDOW% "%CH_TITLE%" CMD /K TITLE %CH_TITLE%
+	call :ClearCHVars & CMD /K TITLE %CH_TITLE% ^& cd /D "%CH_WINDOW_DIR%"
 )
+
+GOTO :DONE_WITH_WINDOW
+
+:OPEN_NEW_WINDOW
+if EXIST "%CH_CONFIG%" (
+	call :ClearCHVars & start "%CH_TITLE%" /D "%CH_WINDOW_DIR%" CMD /K TITLE %CH_TITLE% ^& "%CH_CONFIG%"
+) else (
+	call :ClearCHVars & start "%CH_TITLE%" /D "%CH_WINDOW_DIR%" CMD /K TITLE %CH_TITLE%
+)
+
+:DONE_WITH_WINDOW
 
 REM -------------------------------------------------------------------
 
-:EXITBAT
+:EXIT_BAT
 ENDLOCAL
 GOTO:EOF
 
@@ -60,7 +80,7 @@ set %1=%~f2
 goto:EOF
 
 :ClearCHVars
-set CH_WINDOW=
+set CH_OPEN_NEW_WINDOW=
 set CH_TITLE=
 set CH_CONFIG=
 set CH_WINDOW_DIR=
